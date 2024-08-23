6da340875da7a8fafe2bf50da06410dd423da5d3
griminir <viktor.elliot.king@gmail.com>
2024-08-23 18:42:51 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+"""just testing stuff"""
+print('hello python')
+
+course = 'python programming'
+print(len(course))
+print(course[0])
+print(course[-1])
+print(course[0:3])
+print(course[0:])
+print(course[:3])
+print(course[:])
+
+# escape sequances \" \' \n \\
+first = 'viktor'
+last = 'degray'
+full = f'{first} {last}'
+print(full)
+
+
+x = input("x: ")
+
+y = int(x) + 1
+print(y)

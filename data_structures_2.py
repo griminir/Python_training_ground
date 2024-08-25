b59da93b8c3b9620a5e65a6ccc889096588e5071
griminir <viktor.elliot.king@gmail.com>
2024-08-25 15:26:39 +0200

    added info about sets, tuples and dictionaries

--- data_structures_2.py
+++ data_structures_2.py
@@ -1,5 +1,6 @@
 """other filled started to get to filled"""
 from collections import deque
+from array import array
 # queues (FIFO = First In First Out)
 # the import of deque helps with preformance (example large lists)
 queue = deque([])
@@ -7,7 +8,7 @@ queue.append(1)
 queue.append(2)
 queue.append(3)
 
-queue.popleft() #special deque method
+queue.popleft()  # special deque method
 print(queue)
 if not queue:
     print('empty')
@@ -15,4 +16,41 @@ if not queue:
 
 # tuples can not be modified so use them when you dont want data to get change
 point = (1, 2, 3)
+x = 5
+y = 10
+x, y = y, x
+print(f'y: {y}, x:{
+      x} as you can see they are not the same value as originally stated, they have been swapped')
 
+# arrays better for large list of numbers > 10000 ish (use only if you are having preformance problems)
+# need to be imported
+# need a type code for the array google it lowercase i is for int
+numbers = array('i', [1, 2, 3])
+# works like a list but is strongly typed, the list can only contain items of the correct data type
+
+# sets
+numbers_2 = [1,2,3,4,3,5,5,2,2]
+first = set(numbers_2)
+second = {1, 6, 3}
+# print(first)
+print(first | second) # both sets together but only uniques
+print(first & second) # only items in that is in both sets
+print(first - second) # only items that are in first but not second
+print(first ^ second) # all the items that are unique for either first or second
+# cant use index so we use 
+if 1 in first: 
+    print('yes')
+
+
+# dictioraries are key:value pairs
+point2 = dict(x=1, Y=2)
+print(f'{point2['x']} from dictonary')
+point2['z'] = 20
+point2['x'] = 10
+if 'a' in point2:
+    print(point2['a'])
+print(point2.get('a', 'does not exist'))
+del point2['x']
+print(point2)
+for key in point2:
+    print(key, point2[key])

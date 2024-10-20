50801e38164036ad2056b9a1e1b2e9c9263d8c48
griminir <viktor.elliot.king@gmail.com>
2024-10-20 16:54:26 +0200

    good example of inheritance and abstract class

--- classes_4_inheritance_good_example.py
+++ classes_4_inheritance_good_example.py
@@ -1,8 +1,13 @@
+# abc = abstract base class
+from abc import ABC, abstractmethod
+
+
 # good example use of inheritance
 class InvalidOperationError(Exception):
     pass
 
-class Stream:
+
+class Stream(ABC):
     def __init__(self):
         self.opened = False
 
@@ -16,10 +21,25 @@ class Stream:
             raise InvalidOperationError('Stream is already closed')
         self.opened = False
 
+    # making a method abstract means each class that inherits from this clas needs to implement it
+    @abstractmethod
+    def read(self):
+        pass
+
+
 class FileStream(Stream):
     def read(self):
         print('reading data from a file')
 
+
 class NetworkStream(Stream):
     def read(self):
         print('reading data from a network')
+
+
+class MemoryStream(Stream):
+    def read(self):
+        print('reading data from a memory stream')
+
+
+stream = MemoryStream()
